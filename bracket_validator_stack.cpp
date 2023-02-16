44472e2089c65e0b9b3ed2b5611cebcb8b0470f5
Piyush Ranjan Singh <95350374+Darth-InVader15@users.noreply.github.com>
2023-02-16 19:59:30 +0530

    Debugged

--- bracket_validator_stack.cpp
+++ bracket_validator_stack.cpp
@@ -6,22 +6,25 @@ using namespace std;
 #define fast_cin()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);
-// AFTERGLOW
+
 //Bracket checker
 int main()
 {
     string str;
     cin>>str;
     stack<char> st;
-    int count =0;
+    //int count =0;
     for(auto x : str)
     {
+        //INPUT ANY OPEN BRACKET IN THE STACK
         if(x == '{' || x == '[' || x == '(')
         {
             st.push(x);
         }
         else
         {
+            //ELSE CHECK WHETHER THE LAST OPENED BRACKET IN THE STACK MATCHES THE CURRENT CLOSING BRACKET
+            //ALSO CHECK WHETHER THE STACK IS EMPTY OR NOT IN THE BEGINNING, IN CASE THE I/P IS LIKE ')'
             if(st.empty())
             {
                 cout<<"False\n";
@@ -58,7 +61,10 @@ int main()
                 st.pop();
             }
         }
-       
     }
+    //CHECK WHETHER THE STACK IS EMPTY TO AVOID EDGE CASES LIKE '(){}('
+    if(st.size()==0)
     cout<<"True\n";
+    else 
+    cout<<"False\n";
 }
