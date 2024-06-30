dd5c6ea24a8d30b772d77ce68aa1f26b062c4f4b
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-07-01 03:35:11 +0530

    updated snippet

--- snippet.cpp
+++ snippet.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 #define endl "\n"
 #define ll long long
+#define ull unsigned long long
+#define forn(n) for(int i=0;i<n;i++)
+#define pi pair<int,int>
+#define pii pair<int,pair<int,int>>
 #define pb push_back
 #define fast_cin()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);			\
 	cout.tie(NULL);
-// FEEL IT STILL
-// FLY ON
 
+// FLY ON
+void solve()
+{
+	cout<<"Hello World\n";
+}
 
 int32_t main()
 {
 	fast_cin();
 	
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+		solve();
+	}
 	
-
-
-	
-
 }
